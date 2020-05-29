69828095626943f683d1d177d894a8bcef7707f0
Aby Kuruvilla <abyk888@gmail.com>
2020-05-29 17:37:00 +0530

    spring init

---
+++ src/main/java/com/example/virtualq/VirtualqApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/virtualq/VirtualqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
